80025e9baa5da1ec6a59d2ba1908be977e95d86c
chenjinsongcjs <13887096914@163.com>
2026-03-07 12:26:47 +0800

    Initial Commit

---
+++ Markly/MarklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarklyApp.swift
+//  Markly
+//
+//  Created by 陈进松 on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct MarklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
